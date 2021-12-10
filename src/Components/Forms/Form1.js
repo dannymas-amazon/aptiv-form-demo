ca1dd89770d75af13258f277dfc06eef1bd6d8e3
dannymas-amazon <dannymas@amazon.com>
2021-12-10 17:53:45 -0500

    totals

--- src/Components/Forms/Form1.js
+++ src/Components/Forms/Form1.js
@@ -10,12 +10,12 @@ import {
     Input,
     ColumnLayout,
     DatePicker,
-    Select
+    Select,
+    Grid
 } from "@awsui/components-react/"
 import aptivLogo from '../../aptivLogo.png';
 import {getOperatorById} from "../../api/OperatorApi";
 import {getFNCRecordByFNCNumber} from "../../api/FNCRecordsApi";
-import {getFNCRecords} from "../../graphql/queries";
 
 export default function Form1() {
     const [date, setDate] = useState("");
@@ -30,41 +30,185 @@ export default function Form1() {
     const [boxInputs, setBoxInputs] = useState([]);
     const [selectedOption, setSelectedOption] = useState(null);
 
+    //Totals
+    const [totalBoxQuantity, setTotalBoxQuantity] = useState(0);
+    const [totalDefects, setTotalDefects] = useState(0);
+    const [totalManque, setTotalManque] = useState(0);
+    const [totalBavure, setTotalBavure] = useState(0);
+    const [totalExces, setTotalExces] = useState(0);
+    const [totalAutres, setTotalAutres] = useState(0);
+
     const addRow = () => {
         const newBoxInputs = [...boxInputs, {boxIndex: boxInputs.length + 1, id: "", defect: ""}]
         setBoxInputs(newBoxInputs)
     }
 
-    const changeRowId = (row, value) => {
-        const newBoxInputs = boxInputs.map(i => i.boxIndex === row.boxIndex ? {
-            boxIndex: row.boxIndex,
-            id: value,
-            defect: row.defect
-        } : i)
+    const changeRow = (row, value, field) => {
+        let newBoxInputs
+        let defects
+        switch(field){
+            case 'id':
+                newBoxInputs = boxInputs.map(i => i.boxIndex === row.boxIndex ? {...i, id: value} : i)
+                break;
+            case 'boxDate':
+                newBoxInputs = boxInputs.map(i => i.boxIndex === row.boxIndex ? {...i, boxDate: value} : i)
+                break;
+            case 'boxQuantity':
+                newBoxInputs = boxInputs.map(i => i.boxIndex === row.boxIndex ? {...i, boxQuantity: value} : i)
+                const boxQuantity = newBoxInputs.map(i => {
+                    if(i.boxQuantity && !isNaN(i.boxQuantity.trim()))
+                        return parseInt(i.boxQuantity.trim(), 10)
+                    return 0
+                }).reduce(add, 0)
+                setTotalBoxQuantity(boxQuantity)
+                break;
+            case 'manqueMatiere':
+                newBoxInputs = boxInputs.map(i => {
+                    if(i.boxIndex === row.boxIndex) {
+                        let sum = 0
+                        if(value && !isNaN(value.trim())) sum += parseInt(value.trim(), 10)
+                        if(i.bavure && !isNaN(i.bavure.trim())) sum += parseInt(i.bavure.trim(), 10)
+                        if(i.exces && !isNaN(i.exces.trim())) sum += parseInt(i.exces.trim(), 10)
+                        if(i.autres && !isNaN(i.autres.trim())) sum += parseInt(i.autres.trim(), 10)
+                        return {...i, manqueMatiere: value, defectNumber: sum}
+                    } else {
+                        return i
+                    }
+                })
+                const manqueMatiere = newBoxInputs.map(i => {
+                    if(i.manqueMatiere && !isNaN(i.manqueMatiere.trim()))
+                        return parseInt(i.manqueMatiere.trim(), 10)
+                    return 0
+                }).reduce(add, 0)
+                defects = newBoxInputs.map(i => {
+                    if(i.defectNumber)
+                        return i.defectNumber
+                    return 0
+                }).reduce(add, 0)
+                setTotalDefects(defects)
+                setTotalManque(manqueMatiere)
+                break;
+            case 'bavure':
+                newBoxInputs = boxInputs.map(i => {
+                    if(i.boxIndex === row.boxIndex) {
+                        let sum = 0
+                        if(i.manqueMatiere && !isNaN(i.manqueMatiere.trim())) sum += parseInt(i.manqueMatiere.trim(), 10)
+                        if(value && !isNaN(value.trim())) sum += parseInt(value.trim(), 10)
+                        if(i.exces && !isNaN(i.exces.trim())) sum += parseInt(i.exces.trim(), 10)
+                        if(i.autres && !isNaN(i.autres.trim())) sum += parseInt(i.autres.trim(), 10)
+                        return {...i, bavure: value, defectNumber: sum}
+                    } else {
+                        return i
+                    }
+                })
+                const bavure = newBoxInputs.map(i => {
+                    if(i.bavure && !isNaN(i.bavure.trim()))
+                        return parseInt(i.bavure.trim(), 10)
+                    return 0
+                }).reduce(add, 0)
+                defects = newBoxInputs.map(i => {
+                    if(i.defectNumber)
+                        return i.defectNumber
+                    return 0
+                }).reduce(add, 0)
+                setTotalDefects(defects)
+                setTotalBavure(bavure)
+                break;
+            case 'exces':
+                newBoxInputs = boxInputs.map(i => {
+                    if(i.boxIndex === row.boxIndex) {
+                        let sum = 0
+                        if(i.manqueMatiere && !isNaN(i.manqueMatiere.trim())) sum += parseInt(i.manqueMatiere.trim(), 10)
+                        if(i.bavure && !isNaN(i.bavure.trim())) sum += parseInt(i.bavure.trim(), 10)
+                        if(value && !isNaN(value.trim())) sum += parseInt(value.trim(), 10)
+                        if(i.autres && !isNaN(i.autres.trim())) sum += parseInt(i.autres.trim(), 10)
+                        return {...i, exces: value, defectNumber: sum}
+                    } else {
+                        return i
+                    }
+                })
+                const exces = newBoxInputs.map(i => {
+                    if(i.exces && !isNaN(i.exces.trim()))
+                        return parseInt(i.exces.trim(), 10)
+                    return 0
+                }).reduce(add, 0)
+                defects = newBoxInputs.map(i => {
+                    if(i.defectNumber)
+                        return i.defectNumber
+                    return 0
+                }).reduce(add, 0)
+                setTotalDefects(defects)
+                setTotalExces(exces)
+                break;
+            case 'autres':
+                newBoxInputs = boxInputs.map(i => {
+                    if(i.boxIndex === row.boxIndex) {
+                        let sum = 0
+                        if(i.manqueMatiere && !isNaN(i.manqueMatiere.trim())) sum += parseInt(i.manqueMatiere.trim(), 10)
+                        if(i.bavure && !isNaN(i.bavure.trim())) sum += parseInt(i.bavure.trim(), 10)
+                        if(i.exces && !isNaN(i.exces.trim())) sum += parseInt(i.exces.trim(), 10)
+                        if(value && !isNaN(value.trim())) sum += parseInt(value.trim(), 10)
+                        return {...i, autres: value, defectNumber: sum}
+                    } else {
+                        return i
+                    }
+                })
+                const autres = newBoxInputs.map(i => {
+                    if(i.autres && !isNaN(i.autres.trim()))
+                        return parseInt(i.autres.trim(), 10)
+                    return 0
+                }).reduce(add, 0)
+                defects = newBoxInputs.map(i => {
+                    if(i.defectNumber)
+                        return i.defectNumber
+                    return 0
+                }).reduce(add, 0)
+                setTotalDefects(defects)
+                setTotalAutres(autres)
+                break;
+            default:
+                break;
+        }
         setBoxInputs(newBoxInputs)
     }
 
-    const changeRowDefect = (row, value) => {
-        const newBoxInputs = boxInputs.map(i => i.boxIndex === row.boxIndex ? {
-            boxIndex: row.boxIndex,
-            id: row.id,
-            defect: value
-        } : i)
-        setBoxInputs(newBoxInputs)
+    const add = (accumulator, a) =>{
+        return accumulator + a
     }
 
     let boxInputList = boxInputs.map(i =>
-        <ColumnLayout key={i.boxIndex} columns={3}>
-            <FormField label={"Box Id"}>
-                <Input onChange={({detail}) => changeRowId(i, detail.value)} value={i.id}/>
-            </FormField>
-            <FormField label={"Metadata"}>
-                <Input disabled/>
-            </FormField>
-            <FormField label={"Defects"}>
-                <Input onChange={({detail}) => changeRowDefect(i, detail.value)} value={i.defect}/>
-            </FormField>
-        </ColumnLayout>
+        <Grid key={i.boxIndex} gridDefinition={[{ colspan: 3 }, { colspan: 9 }]}>
+            <ColumnLayout  columns={1}>
+                <FormField label={"Box Id"}>
+                    <Input onChange={({detail}) => changeRow(i, detail.value, 'id')} value={i.id}/>
+                </FormField>
+            </ColumnLayout>
+            <ColumnLayout columns={4}>
+                <FormField style={{minWidth: "10px"}} label={"Box Date"}>
+                    <Input onChange={({detail}) => changeRow(i, detail.value, 'boxDate')} value={i.boxDate}/>
+                </FormField>
+                <FormField label={"Box Quantity"}>
+                    <Input onChange={({detail}) => changeRow(i, detail.value, 'boxQuantity')} value={i.boxQuantity}/>
+                </FormField>
+                <FormField label={"Number of Defects"}>
+                    <Input disabled value={i.defectNumber}/>
+                </FormField>
+                <div/>
+                <FormField label={"Manque Matiere"}>
+                    <Input onChange={({detail}) => changeRow(i, detail.value, 'manqueMatiere')} value={i.manqueMatiere}/>
+                </FormField>
+                <FormField label={"Bavure"}>
+                    <Input onChange={({detail}) => changeRow(i, detail.value, 'bavure')} value={i.bavure}/>
+                </FormField>
+                <FormField label={"Exces Matiere"}>
+                    <Input onChange={({detail}) => changeRow(i, detail.value, 'exces')} value={i.exces}/>
+                </FormField>
+                <FormField label={"Autres"}>
+                    <Input onChange={({detail}) => changeRow(i, detail.value, 'autres')} value={i.autres}/>
+                </FormField>
+            </ColumnLayout>
+        </Grid>
+
     )
 
     const employeeLookup = (value) => {
@@ -190,12 +334,42 @@ export default function Form1() {
                                     <Input disabled value={machineId}/>
                                 </FormField>
                             </ColumnLayout>
-                            {boxInputList}
-                            <div style={{width: "100%"}}>
-                                <div style={{float: "right"}}>
-                                    <Button onClick={addRow} variant="normal">Add Row</Button>
+                            <Container>
+                                <ColumnLayout borders="horizontal" columns={1}>
+                                    {boxInputList}
+                                </ColumnLayout>
+                                <div style={{width: "100%", marginTop: "1em",marginBottom: '3em'}}>
+                                    <div style={{float: "right"}}>
+                                        <Button onClick={addRow} variant="normal">Add Row</Button>
+                                    </div>
                                 </div>
-                            </div>
+                            </Container>
+                            <Container header={
+                                <Header variant="h5">
+                                    Totals
+                                </Header>
+                            }>
+                                <ColumnLayout columns={6}>
+                                    <FormField label="Box Quantity">
+                                        <Input disabled value={totalBoxQuantity}/>
+                                    </FormField>
+                                    <FormField label="Defects">
+                                        <Input disabled value={totalDefects}/>
+                                    </FormField>
+                                    <FormField label="Manque Matiere">
+                                        <Input disabled value={totalManque}/>
+                                    </FormField>
+                                    <FormField label="Bavure">
+                                        <Input disabled value={totalBavure}/>
+                                    </FormField>
+                                    <FormField label="Exces Matiere">
+                                        <Input disabled value={totalExces}/>
+                                    </FormField>
+                                    <FormField label="Autres">
+                                        <Input disabled value={totalAutres}/>
+                                    </FormField>
+                                </ColumnLayout>
+                            </Container>
                         </SpaceBetween>
                     </Container>
                 </Form>
